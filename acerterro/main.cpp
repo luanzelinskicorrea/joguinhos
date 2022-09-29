b540d4ddb0137354ca0671bb90f1c5d4717a98c9
Luan Zelinski Corrêa <78267800+luanzelinskicorrea@users.noreply.github.com>
2022-09-29 16:32:57 -0300

    Versão 0.1b (sem jogatina)
    Adicionado as vidas, munições e quantidade de portas de cada dificuldade

--- acerterro/main.cpp
+++ acerterro/main.cpp
@@ -9,12 +9,14 @@
 using namespace std;
 
 int main(){
-    int nivel_dificuldade, numero_portas;
+    int nivel_dificuldade, numero_portas, qtd_municoes, qtd_vidas;
 
     //Tela de início
     for(int logo=1; logo<=25; logo++){
+
         cout<<"Hunting Madness"<<flush;
         system ("CLS");
+
     }
 
     cout<<"Hunting Madness\n\n";
@@ -30,14 +32,22 @@ int main(){
 
 
     for(int dificuldade=1; dificuldade<=3; dificuldade++){
+
         cout<<dificuldade<<flush;
         cout<<"   "<<flush;
+
         if(dificuldade==1){
+
             cout<<"Fácil"<<flush;
+
         }else if(dificuldade==2){
+
             cout<<"Médio"<<flush;
+
         }else if(dificuldade==3){
+
             cout<<"Difícil\n\n"<<flush;
+
         }
     }
 
@@ -45,14 +55,36 @@ int main(){
     cin>>nivel_dificuldade;
 
     if(nivel_dificuldade <=0){
+
         system("CLS");
         goto seleciona_dificuldade;
+
     }else if(nivel_dificuldade > 3){
+
         system("CLS");
         goto seleciona_dificuldade;
+
     }
 
     //Jogo funcionando em cada dificuldade
+    if(nivel_dificuldade == 1){
 
+        numero_portas = 3;
+        qtd_municoes = 5;
+        qtd_vidas = 3;
+
+    }else if(nivel_dificuldade == 2){
+
+        numero_portas = 5;
+        qtd_municoes = 8;
+        qtd_vidas = 3;
+
+    }else if(nivel_dificuldade ==3){
+
+        numero_portas = 7;
+        qtd_municoes = 5;
+        qtd_vidas = 3;
+
+    }
     return 0;
 }
